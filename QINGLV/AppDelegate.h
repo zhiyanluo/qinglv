93171288387286156a38847cdb44940fa7f8a420
罗志炎 <luozhiyan@luozhiyandeMacBook-Pro.local>
2015-10-25 14:54:39 +0800

    Initial Commit

---
+++ QINGLV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QINGLV
+//
+//  Created by 罗志炎 on 15/10/25.
+//  Copyright (c) 2015年 罗志炎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QINGLV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QINGLV
+//
+//  Created by 罗志炎 on 15/10/25.
+//  Copyright (c) 2015年 罗志炎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
